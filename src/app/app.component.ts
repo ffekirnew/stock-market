adf8de04e88bce496a84bc54568c6b12c4c95fa2
ffekirnew <ffekirnew0808@gmail.com>
2023-03-02 09:51:49 +0300

    added component projection practice files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { Stock } from './model/stock';
 
 @Component({
@@ -6,15 +6,14 @@ import { Stock } from './model/stock';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements  OnInit, OnDestroy,
+                                      AfterContentChecked, AfterContentInit,
+                                      AfterViewChecked, AfterViewInit,
+                                      DoCheck, OnChanges {
   title = 'stock-market';
   public stock: any;
   private counter: number = 1;
-
-  ngOnInit() {
-    this.stock = new Stock("Test Stock Company", "TSC", 85, 90);
-  }
-
+  
   onToggleFavorite(stock: Stock): void {
     console.log(stock);
     stock.favorite = !stock.favorite;
@@ -27,4 +26,38 @@ export class AppComponent implements OnInit {
   changeStockPrice() {
     this.stock.price += 10;
   }
+
+  ngOnInit(): void {
+    console.log("App Component - On Init");
+    this.stock = new Stock("Test Stock Company", "TSC", 85, 80);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("App Component - On Changes");
+  }
+
+  ngDoCheck(): void {
+    console.log("App Component - Do Check");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("App Component - After Content Checked");
+  }
+
+  ngAfterContentInit(): void {
+    console.log("App Component - After Content Init");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("App Component - View Init");
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("App Component - View Checked");
+  }
+
+  ngOnDestroy(): void {
+    console.log("App Component - On Destroy");
+  }
+
 }
--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,13 +1,16 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, ChangeDetectionStrategy, Component, DoCheck, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Stock } from 'src/app/model/stock';
 
 @Component({
   selector: 'app-stock-item',
   templateUrl: './stock-item.component.html',
   styleUrls: ['./stock-item.component.css'],
-  changeDetection: ChangeDetectionStrategy.Default,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StockItemComponent {
+export class StockItemComponent implements  OnInit, OnDestroy,
+                                            AfterContentChecked, AfterContentInit,
+                                            AfterViewChecked, AfterViewInit,
+                                            DoCheck, OnChanges {
   @Input()
   public stock: any;
 
@@ -26,4 +29,38 @@ export class StockItemComponent {
   changeStockPrice() {
     this.stock.price += 5;
   }
+
+  ngOnInit(): void {
+    console.log("Stock Item Component - On Init");
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("Stock Item Component - On Changes");
+  }
+
+  ngDoCheck(): void {
+    console.log("Stock Item Component - Do Check");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("Stock Item Component - After Content Checked");
+  }
+
+  ngAfterContentInit(): void {
+    console.log("Stock Item Component - After Content Init");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("Stock Item Component - View Init");
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("Stock Item Component - View Checked");
+  }
+
+  ngOnDestroy(): void {
+    console.log("Stock Item Component - On Destroy");
+  }
+
+
 }
